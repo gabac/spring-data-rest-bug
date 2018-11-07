df81f72030b7d1fa54614616934492b58de6e176
Cyril Gabathuler <cyril@3ap.ch>
2018-11-07 08:30:04 +0100

    Enable original address again

--- demo-app/person-service/src/main/java/com/example/spring/bug/demoapp/DemoAppApplication.java
+++ demo-app/person-service/src/main/java/com/example/spring/bug/demoapp/DemoAppApplication.java
@@ -3,6 +3,8 @@ package com.example.spring.bug.demoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +12,11 @@ public class DemoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
+	}
+	
+
+	@Bean
+	public ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
 	}
 }
