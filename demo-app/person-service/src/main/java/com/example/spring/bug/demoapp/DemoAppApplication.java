4e6bca7063490d15cd47595c259731d87b725f12
Cyril Gabathuler <cyril@3ap.ch>
2018-11-07 09:30:31 +0100

    doesnt work yet with config

--- demo-app/person-service/src/main/java/com/example/spring/bug/demoapp/DemoAppApplication.java
+++ demo-app/person-service/src/main/java/com/example/spring/bug/demoapp/DemoAppApplication.java
@@ -3,8 +3,6 @@ package com.example.spring.bug.demoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,11 +10,5 @@ public class DemoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
-	}
-	
-
-	@Bean
-	public ForwardedHeaderFilter forwardedHeaderFilter() {
-		return new ForwardedHeaderFilter();
 	}
 }
